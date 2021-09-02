54fa6720aceaaaea72560f27f209fcd11202fd7b
Sebastian <sebascantini@gmail.com>
2021-09-02 16:13:26 -0300

    notas

--- src/dinamicPrograming.cpp
+++ src/dinamicPrograming.cpp
@@ -1,5 +1,22 @@
 #include "dinamicPrograming.h"
 
-int dinamicPrograming(info_t &info){
-    return 0;
+estructura memo;
+
+void aux(info_t &info){ // n veces
+    aux(prox nodo) // o(1)
+    alocar ingo en memo // o(m)
 }
+
+
+int dinamicPrograming(info_t &info){
+    aux(info); // o(m*n)
+    int ans = conseguir_max(memo); // o(m)
+    return ans;
+}
+
+
+f(i,){
+    
+
+}
+
