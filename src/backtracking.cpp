1680670fbb084affa05aac0a7d57ea5e5e9fa055
Sebastian <sebascantini@gmail.com>
2021-09-11 15:24:41 -0300

    bugfix

--- src/backtracking.cpp
+++ src/backtracking.cpp
@@ -33,8 +33,8 @@ int backtracking(const info_t &info, const int mode, int i, int riesgo, int bene
     return -1;                                           // O(1)
 
   //recursion
-  int b1 = backtracking(info, mode, i+1, riesgo, beneficio, potencial); //no nos quedamos con este nodo                                            // O(1)
-  int b2 = backtracking(info, mode, i+2, riesgo + info.contagios[i], beneficio + info.beneficios[i], potencial - (potencial != 0)?info.beneficios[i+1]:0); //si nos quedamos con este nodo   // O(1)
+  int b1 = backtracking(info, mode, i+1, riesgo, beneficio, potencial); //no nos quedamos con este nodo                                       // O(1)
+  int b2 = backtracking(info, mode, i+2, riesgo + info.contagios[i], beneficio + info.beneficios[i], potencial - ((potencial != 0)?info.beneficios[i+1]:0)); //si nos quedamos con este nodo   // O(1)
   return max(b1, b2);                                                                                                                              // O(1)
 }
 
