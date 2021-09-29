7403fabc6289870b52840857cd9eb7df80dc392a
Sebastian <sebascantini@gmail.com>
2021-09-29 20:31:59 -0300

    Top-Down DP Implementation

--- src/backtracking.cpp
+++ src/backtracking.cpp
@@ -10,17 +10,13 @@
 
 int mejorBeneficio = 0;
 
-int backtracking(const info_t &info, const int mode, int i, int riesgo, int beneficio, int potencial){
+int backtrackingAux(const info_t &info, const int mode, int potencial, int i = 0, int riesgo = 0, int beneficio = 0){
   //caso base
   if(i >= info.cantidad){                                     // O(1)
     beneficio = (riesgo <= info.limite) ? beneficio : 0;      // O(1)
     mejorBeneficio = max(beneficio, mejorBeneficio);          // O(1)
     return beneficio;                                         // O(1)
   }
-
-  if(i == 0)                                              // O(1)
-    for(int j = 0; j < info.cantidad; ++j)                // O(n)
-      potencial += info.beneficios[j];                    // O(1)
   
   //poda por optimalidad
   if(mode % 2 == 0 && beneficio + potencial < mejorBeneficio)       // O(1)   modo 0 o 2 incluye optimalidad
@@ -33,9 +29,16 @@ int backtracking(const info_t &info, const int mode, int i, int riesgo, int bene
     return -1;                                           // O(1)
 
   //recursion
-  int b1 = backtracking(info, mode, i+1, riesgo, beneficio, potencial); //no nos quedamos con este nodo                                       // O(1)
-  int b2 = backtracking(info, mode, i+2, riesgo + info.contagios[i], beneficio + info.beneficios[i], potencial - ((potencial != 0)?info.beneficios[i+1]:0)); //si nos quedamos con este nodo   // O(1)
+  int b1 = backtrackingAux(info, mode, potencial, i+1, riesgo, beneficio); //no nos quedamos con este nodo                                       // O(1)
+  int b2 = backtrackingAux(info, mode, potencial - ((potencial != 0)?info.beneficios[i+1]:0), i+2, riesgo + info.contagios[i], beneficio + info.beneficios[i]); //si nos quedamos con este nodo   // O(1)
   return max(b1, b2);                                                                                                                              // O(1)
+}
+
+int backtracking(const info_t &info, const int mode){
+  int potencial = 0;
+  for(int j = 0; j < info.cantidad; ++j)                // O(n)
+    potencial += info.beneficios[j];                    // O(1)
+  return backtrackingAux(info, mode, potencial);
 }
 
 // O(2^n + n) = O(2^n) (el 2^n+n se debe a que ese n proviene de calcularSufijos, la cual corre solo una vez cuando i = 0 y cuando mode es par)
--- src/dinamicPrograming.cpp
+++ src/dinamicPrograming.cpp
@@ -2,41 +2,93 @@
 
 vector<vector<int>> memo;
 
-//n: cantidad de tiendas totales
-//m: maximo de contagios
+int dinamicProgramingAux(const info_t &info, int i = 0, int riesgo = 0){
+  //caso base
+  if(riesgo > info.limite)        //O(1)
+    return -1;                    //O(1)
 
-void dinamicProgramingAux(const info_t &info, int i){
-//caso base
-  if(i >= info.cantidad)                                                                            //O(1)
-      return;                                                                                       //O(1)
+  if(i >= info.cantidad)          //O(1)
+    return 0;                     //O(1)
 
-//guardar en memo
+  if(memo[i][riesgo] != -1)
+    return memo[i][riesgo];
 
-//IMPORTANTE: vamos a usar solo tres filas para la matriz, por lo que la asignacion de valores sera en la fila modulo 3
+  //recursion
+  int b1 = dinamicProgramingAux(info, i+1, riesgo); //no nos quedamos con este nodo
+  int b2 = dinamicProgramingAux(info, i+2, riesgo + info.contagios[i]); //si nos quedamos con este nodo
 
-//limpiamos fila actual
-int j = i % 3; // fila                                                                            // O(1)
-memo[j] = vector<int>(info.limite+1, 0);                                                          // O(m)
-
-//primero guardamos elemento actual
-memo[j][info.contagios[i]] = info.beneficios[i];                                                  // O(1)
-
-//luego calculamos combinaciones con fila j-2
-int k = (j+1) % 3; // fila j-2 para combinaciones // (seria j-2%3)                                // O(1)
-for(int l = 1; l <= info.limite-info.contagios[i]; ++l)                                           // O(m) 
-  memo[j][l+info.contagios[i]] = memo[k][l] + info.beneficios[i];                                 // O(1)
-
-//shifteo de la fila j-1 hacia j, quedandonos solo con los elementos mas grandes de la columna
-k = (j+2) % 3;                                    // (seria j-1%3)                                // O(1)
-for(int l = 1; l <= info.limite; ++l)                                                             // O(m)
-  memo[j][l] = max(memo[j][l], memo[k][l]);                                                       // O(1)
-
-//recursion
-  dinamicProgramingAux(info , i+1); //no nos quedamos con este nodo                                 // O(3m+6) = O(m) 
-}                                                                                                   // TOTAL: O(n*m)
+  int ans = max(b1, (b2 != -1) ? b2 + info.beneficios[i] : 0);
+  
+  memo[i][riesgo] = ans;
+  
+  return ans;                   // O(1)
+}
 
 int dinamicPrograming(const info_t &info){
-  memo = vector<vector<int>>(3, vector<int>(info.limite+1, 0));                                 // usamos max_contagio+1 para simplificar el uso de indices en la matriz             // O(3*m) = O(m)    
-  dinamicProgramingAux(info, 0);                                                                // O(m*n) debido a que llamamos a los sumo n veces (0<=i<=n) a dinamicProgramming recursivamente, la cual es   // O(m)
-  return *max_element(memo[(info.cantidad-1)%3].begin(), memo[(info.cantidad-1)%3].end());      // calcular indice de memo para devolver max de esa fila // O(m)
-}                                                                                               // TOTAL: O(m*n)
+  memo = vector<vector<int>>(info.cantidad, vector<int>(info.limite+1, -1));   // usamos max_contagio+1 para simplificar el uso de indices en la matriz    // O(n*m) 
+  return dinamicProgramingAux(info);                                  // O(m*n) debido a que llamamos a los sumo n veces (0<=i<=n) a dinamicProgramming recursivamente, la cual es
+}  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include "dinamicPrograming.h"
+
+// vector<vector<int>> memo;
+
+// //n: cantidad de tiendas totales
+// //m: maximo de contagios
+
+// void dinamicProgramingAux(const info_t &info, int i){
+// //caso base
+//   if(i >= info.cantidad)                                                                            //O(1)
+//       return;                                                                                       //O(1)
+
+// //guardar en memo
+
+// //IMPORTANTE: vamos a usar solo tres filas para la matriz, por lo que la asignacion de valores sera en la fila modulo 3
+
+// //limpiamos fila actual
+// int j = i % 3; // fila                                                                            // O(1)
+// memo[j] = vector<int>(info.limite+1, 0);                                                          // O(m)
+
+// //primero guardamos elemento actual
+// memo[j][info.contagios[i]] = info.beneficios[i];                                                  // O(1)
+
+// //luego calculamos combinaciones con fila j-2
+// int k = (j+1) % 3; // fila j-2 para combinaciones // (seria j-2%3)                                // O(1)
+// for(int l = 1; l <= info.limite-info.contagios[i]; ++l)                                           // O(m) 
+//   memo[j][l+info.contagios[i]] = memo[k][l] + info.beneficios[i];                                 // O(1)
+
+// //shifteo de la fila j-1 hacia j, quedandonos solo con los elementos mas grandes de la columna
+// k = (j+2) % 3;                                    // (seria j-1%3)                                // O(1)
+// for(int l = 1; l <= info.limite; ++l)                                                             // O(m)
+//   memo[j][l] = max(memo[j][l], memo[k][l]);                                                       // O(1)
+
+// //recursion
+//   dinamicProgramingAux(info , i+1); //no nos quedamos con este nodo                                 // O(3m+6) = O(m) 
+// }                                                                                                   // TOTAL: O(n*m)
+
+// int dinamicPrograming(const info_t &info){
+//   memo = vector<vector<int>>(3, vector<int>(info.limite+1, 0));                                 // usamos max_contagio+1 para simplificar el uso de indices en la matriz             // O(3*m) = O(m)    
+//   dinamicProgramingAux(info, 0);                                                                // O(m*n) debido a que llamamos a los sumo n veces (0<=i<=n) a dinamicProgramming recursivamente, la cual es   // O(m)
+//   return *max_element(memo[(info.cantidad-1)%3].begin(), memo[(info.cantidad-1)%3].end());      // calcular indice de memo para devolver max de esa fila // O(m)
+// }                                                                                               // TOTAL: O(m*n)
